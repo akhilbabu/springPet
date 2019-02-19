35cb4ac4a1656f6b67b32eee0b287eb1beedea6d
akhilbabu <akhilbabu92@gmail.com>
2019-02-19 19:24:07 +0530

    fisrt stable version with welcome message

--- src/main/java/com/SpringPet/clinic/controller/Welcome.java
+++ src/main/java/com/SpringPet/clinic/controller/Welcome.java
@@ -3,8 +3,10 @@ package com.SpringPet.clinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class Welcome {
 	
 	
