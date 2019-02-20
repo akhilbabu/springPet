216c6b1558b1fa07d91d7aba637543602a82a1fe
akhilbabu <akhilbabu92@gmail.com>
2019-02-20 15:37:20 +0530

    added log in functionlity as well as home page

--- src/main/java/com/SpringPet/clinic/controller/login.java
+++ src/main/java/com/SpringPet/clinic/controller/login.java
@@ -2,9 +2,15 @@ package com.SpringPet.clinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.SpringPet.clinic.model.UserDTO;
 
 @Controller
 public class login {
@@ -18,18 +24,27 @@ public class login {
 	}
 	
 	
-	@RequestMapping("/home")
-	public String home()
+	@GetMapping("/login")
+	public String home(Model model)
 	{
+		UserDTO userdto=new  UserDTO("","");
+		model.addAttribute("userdto", userdto);
+		
 		return "login";
 	}
 
 	
 	
-	@RequestMapping("/login")
-	public void login(Model model)
+	@PostMapping("/login")
+	public ModelAndView checkLogin(@ModelAttribute UserDTO userdto)
 	{
-		System.out.println("user logged in");
+		System.out.println(userdto.getUsername());
+		System.out.println(userdto.getPassword());
+		ModelAndView mAndV=new ModelAndView();
+		mAndV.setViewName("result");
+		mAndV.addObject("username", userdto.getUsername());
+		return mAndV;
+		
 	}
 	
 }
